3c48750034d8b8ae0d93d47da828f128794fd3d3
khanhvinguyen <132880086+khanhvinguyen@users.noreply.github.com>
2023-07-17 21:08:24 +0700

    Create action-update.php

---
+++ btvnb11/product/action-update.php
@@ -0,0 +1,13 @@
+<?php
+    require_once "pdo.php";
+    $productConnection = new ProductConnection();
+    $data = [
+        'prodName' => $_POST['name'],
+        'prodPrice' => $_POST['price'],
+        'cateId' => $_POST['cateId'],
+        'id' => $_GET['id']
+    ];
+    $productConnection->updateProdData($data);
+    header("Location: http://localhost/btvnb11/product/index.php");
+?>
+
