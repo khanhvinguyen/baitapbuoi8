c5821bd1fe870f7de300677dc96c4605d556efd6
khanhvinguyen <132880086+khanhvinguyen@users.noreply.github.com>
2023-07-17 21:08:38 +0700

    Create delete.php

---
+++ btvnb11/product/delete.php
@@ -0,0 +1,9 @@
+
+<?php
+    require_once 'pdo.php';
+    $productConnection = new ProductConnection();
+    $id = ['id' => $_POST['id']];
+    $productConnection->deleteProdData($id);
+    header("Location: http://localhost/btvnb11/product/index.php");
+?>
+
