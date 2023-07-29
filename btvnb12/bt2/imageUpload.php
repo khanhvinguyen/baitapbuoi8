d5220761a1e2d6c77aa5a9b1eebebc3606d0553b
khanhvinguyen <132880086+khanhvinguyen@users.noreply.github.com>
2023-07-29 08:37:48 +0700

    Create imageUpload.php

---
+++ btvnb12/bt2/imageUpload.php
@@ -0,0 +1,22 @@
+﻿<?php
+class ImageUpload {
+    private $targetDir;
+
+    public function __construct($targetDir) {
+        $this->targetDir = $targetDir;
+    }
+
+
+    public function checkFile($file) {
+ 
+        $allowedFormats = array("jpg", "jpeg", "png", "gif");
+        $fileExtension = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
+
+        if (!in_array($fileExtension, $allowedFormats)) {
+            return "File not valid. Only image files are allowed to be uploaded (jpg, jpeg, png, gif).";
+        }
+
+        return ""; 
+    }
+}
+?>
