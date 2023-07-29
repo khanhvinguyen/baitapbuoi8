f6e104a19c46bae137530680bd96b673c1a80c2a
khanhvinguyen <132880086+khanhvinguyen@users.noreply.github.com>
2023-07-29 08:37:56 +0700

    Create upload.php

---
+++ btvnb12/bt2/upload.php
@@ -0,0 +1,17 @@
+﻿<?php
+require_once 'ImageUpload.php';
+
+$targetDir = "images"; 
+$imageUpload = new imageUpload($targetDir);
+
+
+$uploadedFiles = array_diff(scandir($targetDir), array('..', '.'));
+if (!empty($uploadedFiles)) {
+    echo "<h2>Image :</h2>";
+    echo "<div id='uploaded-image'>";
+    foreach ($uploadedFiles as $file) {
+        echo "<img src='$targetDir/$file' alt='image'>";
+    }
+    echo "</div>";
+}
+?>
