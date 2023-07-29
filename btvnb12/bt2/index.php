d1516a420cff720b29f715dd402421ff775875f6
khanhvinguyen <132880086+khanhvinguyen@users.noreply.github.com>
2023-07-29 08:37:51 +0700

    Create index.php

---
+++ btvnb12/bt2/index.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload File PHP</title>
+</head>
+<body>
+    <h1>Upload Image</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="image" id="image">
+        <br>
+        <input type="submit" name="submit" value="upload">
+    </form>
+
+    <div id="uploaded-image">
+    </div>
+</body>
+</html>
